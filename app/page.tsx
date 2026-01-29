919ee9431092e1f53fff36cb6c254c0d99547a32
Edward Wenkai Liu <liuedward180@gmail.com>
2026-01-29 20:45:55 +1100

    Finished styling events cards and uploaded some dummy data to view how the page looks

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+import { events } from "@/lib/constants";
+
 export default function Home() {
   return (
     <section>
@@ -7,6 +11,23 @@ export default function Home() {
       <p className="text-center mt-5">
         Hackathons, Meetups and Conferences, All in One Place
       </p>
+      <ExploreBtn />
+      <div className="mt-20 space-y-5">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {events.map((event) => (
+            <EventCard
+              key={event.title}
+              title={event.title}
+              image={event.image}
+              slug={event.slug}
+              location={event.location}
+              date={event.date}
+              time={event.time}
+            />
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
